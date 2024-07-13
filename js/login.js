acef215a3767c4f91a431ffada0ce57fbaf0c836
pieterjansen8 <162340980+pieterjansen8@users.noreply.github.com>
2024-07-13 13:05:38 +0200

    Fixed stuff in the hash algorithm

--- js/login.js
+++ js/login.js
@@ -1,4 +1,9 @@
 const url = "https://pieterapi-c8b9e-default-rtdb.europe-west1.firebasedatabase.app/"
+function hash(string) {
+    var str = CryptoJS.SHA256(string)
+    var to_str =str.toString()
+    return to_str
+}
 function setup(){
     const logged = localStorage.getItem("logged")
     if(logged=='true'){
@@ -8,7 +13,8 @@ function setup(){
 setup()
 async function get_acc(){
     const email = document.getElementById("email").value
-    const valid_email = email.replace(".", "")
+    const hashed_mail = hash(email) 
+    const valid_email = hashed_mail.replace(".", "")
     const req = await fetch(url+valid_email+".json")
     const json = await req.json()
     if(json==null){
@@ -22,13 +28,15 @@ async function get_acc(){
 async function login(){
     const acc_exist = await get_acc()
     const password = document.getElementById("password").value 
-    const email = document.getElementById("email").value 
-    const valid_email = email.replace(".", "")
+    const email = document.getElementById("email").value
+    const hashed_mail = hash(email) 
+    const valid_email = hashed_mail.replace(".", "")
+    const password_hashed = hash(password)
     if(acc_exist==true){
         const req = await fetch(url+valid_email+".json")
         const json = await req.json()
-        if(json.password==password){
-            localStorage.setItem("email", email)
+        if(json.password==password_hashed){
+            localStorage.setItem("email", hashed_mail)
             localStorage.setItem("password", password)
             localStorage.setItem("logged", true)
             get_in_acc()
@@ -39,7 +47,7 @@ async function login(){
         }
     }
     const body = JSON.stringify({
-        "password": password, 
+        "password": password_hashed, 
         "files":{
             "num":0 
         }
