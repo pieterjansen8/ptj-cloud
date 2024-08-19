5f65e912a86c451364893ad57ad2f570d64f721e
pieter jansen <pieterjansen8@icloud.com>
2024-08-19 18:11:23 +0200

    semi-switched to supabase

--- js/login.js
+++ js/login.js
@@ -1,5 +1,9 @@
 const url = "https://pieterapi-c8b9e-default-rtdb.europe-west1.firebasedatabase.app/"
 const { createClient } = supabase
+const {dotenv_config} = dotenv 
+
+
+console.log(process.env.APIKEY)
 function hash(string) {
     var str = CryptoJS.SHA256(string)
     var to_str =str.toString()
@@ -67,7 +71,7 @@ async function login(){
             return
         }
     }
-    const _supabase = createClient('https://jupjgyhsopjypuwltlhd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1cGpneWhzb3BqeXB1d2x0bGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwMDg2MDUsImV4cCI6MjAzOTU4NDYwNX0.y3NooSMu4rGYEytT8Yrb1tAV2XfQ9aGGC5IKZPWU8RU')
+    const _supabase = createClient('https://jupjgyhsopjypuwltlhd.supabase.co', process.env.APIKEY)
     const { data, error } = await _supabase.storage.from('files').upload(valid_email+'/welcome.txt', {
         cacheControl: '3600',
         upsert: false
